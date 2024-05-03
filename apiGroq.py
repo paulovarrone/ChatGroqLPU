0754e6334a8b70cd7a29da826442235c824743b4
paulovarrone <paulorobertovarrone@gmail.com>
2024-05-03 09:39:41 -0300

    extract text file dir

--- apiGroq.py
+++ apiGroq.py
@@ -1,29 +1,65 @@
 from groq import Groq
 import PyPDF2
+import os
+
+#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
+
+api_key = "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"
+
+client = Groq(api_key="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")
+
+def extract_text_from_pdf(pdf_file):
+  pdf_text = ""
+  pdf_reader = PyPDF2.PdfReader(pdf_file)
+  for page in pdf_reader.pages:
+    pdf_text += page.extract_text()
+  return pdf_text
 
 
+def resposta(caminho_pdf, caminho_resumos):
+  caminho_pasta = os.path.isdir(caminho_pdf)
+  conteudo_pasta = os.listdir(caminho_pdf)
 
-api_key = "chave api"
+  if caminho_pasta:
+    # Criar a pasta "resumos" se ela não existir
+    if not os.path.exists(caminho_resumos):
+      os.makedirs(caminho_resumos)
 
-client = Groq(api_key="chave api")
+    for arquivo in conteudo_pasta:
+      caminho_arquivos = os.path.join(caminho_pdf, arquivo)
+      file_path = caminho_arquivos
+      
+      if file_path.endswith('.pdf'):
+        
+        text = extract_text_from_pdf(file_path)
 
-# def extract_text_from_pdf(pdf_file):
-#   pdf_text = ""
-#   pdf_reader = PyPDF2.PdfReader(pdf_file)
-#   for page in pdf_reader.pages:
-#     pdf_text += page.extract_text()
-#   return pdf_text
+        completion = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "Você é especialista em resumir informações de documentos e extrair minuciosamente as informações pedidas."},
+            {"role": "user", "content": "Resuma o arquivo."},
+            {"role": "assistant", "content": text}
+          ],
+        model="mixtral-8x7b-32768",
+        temperature=0,
+    )
+                
+        resumo = completion.choices[0].message.content
+
+        # Nome do arquivo de texto para salvar o resumo
+        txt_file_name = os.path.splitext(arquivo)[0] + "_resumo.txt"
+
+        # Caminho completo para o arquivo de texto na pasta "resumos"
+        txt_file_path = os.path.join(caminho_resumos, txt_file_name)
+
+        # Escreve o resumo no arquivo de texto
+        with open(txt_file_path, 'w', encoding='utf-8') as txt_file:
+          txt_file.write(resumo)
+
+# Caminho PDF
+caminho_pdf = r'C:\Users\3470622\Desktop\Workspace\pgm testes\pdfs\DirTrein'
+
+# Caminho resumos
+caminho_resumos = r'C:\Users\3470622\Desktop\Workspace\pgm testes\ResumoPDF sem api\resumos'
 
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Me de uma receita de pão",
-        }
-    ],
-    model="llama3-70b-8192",
-    temperature=0,
-)
-
-print(chat_completion.choices[0].message.content)
+resposta(caminho_pdf, caminho_resumos)
