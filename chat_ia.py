05d2faf5a953d72ed1e73544c7176bf1f596fb8f
paulovarrone <paulorobertovarrone@gmail.com>
2024-05-06 11:11:58 -0300

    chat function

---
+++ chat_ia.py
@@ -0,0 +1,51 @@
+from groq import Groq
+import os
+from extrair_texto import extract_text_from_pdf
+from texto_data import conteudo, pergunta
+
+
+api_key = "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"
+
+client = Groq(api_key = api_key)
+
+def arquivo_texto(caminho_pdf, camininho_contestacao, conteudo_pasta,pasta_contestacao):
+  try: 
+    if not pasta_contestacao:
+      os.makedirs(camininho_contestacao)
+      print("Pasta criada com sucesso.")
+    else:
+      print("Pasta já existe.")
+
+    for arquivo in conteudo_pasta:
+
+      if arquivo.endswith('.pdf'):
+        pdf_file = os.path.join(caminho_pdf, arquivo)
+        text = extract_text_from_pdf(pdf_file)
+
+        
+        chat_completion = client.chat.completions.create(
+            messages=[
+                    {"role": "system", "content": conteudo},
+                    {"role": "user", "content": pergunta},
+                    {"role": "assistant", "content": text}
+                  ],
+                model="llama3-70b-8192",
+                temperature=0,
+                max_tokens=1024,
+                top_p=1,
+                stop=None,
+        )
+
+        texto_da_contestacao = chat_completion.choices[0].message.content
+
+        nome_arquivo_texto = "Contestação_" + os.path.splitext(arquivo)[0] + ".txt"
+
+        caminho_texto = os.path.join(camininho_contestacao, nome_arquivo_texto)
+
+        with open(caminho_texto, 'w', encoding='utf-8') as texto:
+          texto.write(texto_da_contestacao)
+
+        print("Contestação criada com sucesso!")
+
+  except Exception as e:
+    print(f"ERRO {arquivo}: {str(e)}")
